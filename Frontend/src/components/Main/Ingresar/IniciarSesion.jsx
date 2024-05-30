b96172e35d518c706a143a8aacf738b993e3827f
Geider Frias <geiderde20@gmail.com>
2024-05-30 20:17:07 -0300

    hice el diseño del componente login

--- Frontend/src/components/Main/Ingresar/IniciarSesion.jsx
+++ Frontend/src/components/Main/Ingresar/IniciarSesion.jsx
@@ -1,11 +1,25 @@
-// import styles from './Ingresar.module.css'
+import styles from './Ingresar.module.css'
 import { NavLink } from "react-router-dom"
-
-const IniciarSesion = () => {
+function IniciarSesion (){
+  const handleForm = (e)=> {e.preventDefault()}
   return (
-    <div>
+    <div className={styles.container} >
+      <div>
       <h2>INICIAR SESION</h2>
+      <p>Continuar con google</p>
+      <form onSubmit={handleForm} className={styles.form} >
+        <input type="email" placeholder="Correo electronico" className={styles.standar}
+        />
+        <input type="password" placeholder="Contraseña" className={styles.standar}
+        />
+        <button type="submit" className={styles.btn}>Ingresar</button>
+        <button type="submit" className={styles.standar}>Olvide mi contraseña</button>
+      </form>
       <NavLink to="/Registro">¿No tienes una cuenta?. Crear una cuenta</NavLink>
+      </div>
+      <div className={styles.content_img}>
+        <img src="https://rightpeoplegroup.com/wp-content/uploads/2022/11/software-production-team-2021-09-24-04-17-44-utc-min-scaled.jpg" alt="equipo desarrollando" className={styles.img} />
+      </div>
     </div>
   )
 }
--- Frontend/src/components/Main/Inicio/Inicio.jsx
+++ Frontend/src/components/Main/Inicio/Inicio.jsx
@@ -1,9 +1,10 @@
 // import styles from './Inicio.module.css'
-
+import IniciarSesion from "../Ingresar/IniciarSesion"
 const Inicio = () => {
   return (
     <div>
       <h2>INICIO</h2>
+      <IniciarSesion/>
     </div>
   )
 }
