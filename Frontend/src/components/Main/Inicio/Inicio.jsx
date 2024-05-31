4e515ba9212895e7038703e25297bc0fa9fb5895
Geider Frias <geiderde20@gmail.com>
2024-05-30 22:15:28 -0300

    cree el diseño del componente register

--- Frontend/src/components/Main/Inicio/Inicio.jsx
+++ Frontend/src/components/Main/Inicio/Inicio.jsx
@@ -1,10 +1,12 @@
 // import styles from './Inicio.module.css'
 import IniciarSesion from "../Ingresar/IniciarSesion"
+import Registro from "../Ingresar/Registro"
 const Inicio = () => {
   return (
     <div>
       <h2>INICIO</h2>
       <IniciarSesion/>
+      {/* <Registro/> */}
     </div>
   )
 }
