d42e247b4e0319fd8d92ae80e83db095bd801028
Geider Frias <geiderde20@gmail.com>
2024-06-01 20:34:30 -0300

    agregue validadcion con react-hook-forms al componente registro

--- Frontend/src/components/Ingresar/Registro.jsx
+++ Frontend/src/components/Ingresar/Registro.jsx
@@ -1,43 +1,73 @@
 import styles from './Ingresar.module.css'
-import { useForm} from "react-hook-form"
+import { useForm } from "react-hook-form"
 import { NavLink } from 'react-router-dom';
 
 
-function IniciarSesion (){
-  const { register, handleSubmit } = useForm();
-  
-  const handleForm = (e) => {
-    e.preventDefault()
-  }
+function IniciarSesion() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
+  const onSubmit = (data) => {
+    // console.log(data);
+    const { nombre, edad, email, telefono, direccion, password} = data;
+    console.log(nombre);
+  }
+   console.log("errors", errors);
   return (
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.form} >
+      <input type="text" placeholder="Nombre" className={styles.standar}
+        {...register("nombre", {
+          required: { value: true, message: "Nombre requerido" },
+          minLength: { value: 3, message: "Minimo tres digitos" }
+        })}
+      />
+      {errors.nombre && <p>{errors.nombre.message}</p>}
+      <input type="text" placeholder="Edad" className={styles.standar}
+        {...register("edad", {
+          required: { value: true, message: "Edad requerida" },
+          minLength: { value: 1, message: "Minimo uno digito" },
+          maxLength: { value: 2, message: "Maximo dos digitos" },
+        })}
+      />
+      {errors.edad && <p>{errors.edad.message}</p>}
+      <input type="text" placeholder="Direccion" className={styles.standar}
+        {...register("direccion", {
+          required: { value: true, message: "Direccion requerida" },
+          minLength: { value: 3, message: "Minimo tres digitos" }
+        })}
+      />
+      {errors.direccion && <p>{errors.direccion.message}</p>}
+      <input type="text" placeholder="Telefono" className={styles.standar}
+        {...register("telefono", {
+          required: { value: true, message: "Telefono requerida" },
+          maxLength: { value: 20, message: "Maximo 20 digitos" }
+        })}
+      />
+      {errors.telefono && <p>{errors.telefono.message}</p>}
+      <input type="email" placeholder="Correo electronico" className={styles.standar}
+        {...register("email", {
+          required: { value: true, message: "Email requerido" },
+          pattern: { value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/, message: "Formato incorrecto" }
+        })}
+      />
+      {errors.email && <p>{errors.email.message}</p>}
+      <input type="password" placeholder="Contraseña" className={styles.standar}
+        {...register("password", {
+          required: { value: true, message: "Contraseña requerido" },
+          pattern: { value: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/, message: "caracteres entre 8 y 6, al menos un digito, mayuscula, minuscula" }
+        })}
+      />
+      {errors.password && <p>{errors.password.message}</p>}
+
       <div>
-      <form onSubmit={handleForm} className={styles.form} >
-        <input type="text" placeholder="Nombre" className={styles.standar}
+        <input type="checkbox"
+          {...register("check", { required: true })}
         />
-        <input type="text" placeholder="Edad" className={styles.standar}
-        />
-        <input type="text" placeholder="Direccion" className={styles.standar}
-        />
-        <input type="text" placeholder="Telefono" className={styles.standar}
-        />
-        <input type="email" placeholder="Correo electronico" className={styles.standar}
-        />
-        <input type="password" placeholder="Contraseña" className={styles.standar}
-        />
-        <div>
-          <input type="checkbox" 
-          />
-          <label htmlFor="terminos">Acepto los terminos y condiciones</label>
-        </div>
-        <button type="submit" className={styles.btn}>Crear cuenta</button>
-        <NavLink to="/Iniciar-sesion">¿Ya tienes una cuenta?.</NavLink>
-     
-      </form>
-      {/* <div className={styles.content_img}>
-        <img src="https://rightpeoplegroup.com/wp-content/uploads/2022/11/software-production-team-2021-09-24-04-17-44-utc-min-scaled.jpg" alt="equipo desarrollando" className={styles.img} />
-      </div> */}
-    </div>
+        <label htmlFor="terminos">Acepto los terminos y condiciones</label>
+      </div>
+      <button type="submit" className={styles.btn}>Crear cuenta</button>
+      <NavLink to="/Iniciar-sesion">¿Ya tienes una cuenta?.</NavLink>
+    </form>
+
   )
 }
 
