333069ed8013ee2cb26c2ec103818d9ada5f692b
Luis Barrantes <luiyibarrantes@gmail.com>
2024-06-06 19:11:01 -0300

    Change the order of code line 10 to 2 dotenv config and line 17 corsoptions commented

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 const cors = require('cors');
 const passport = require('./auth/auth');
 const userRoutes = require('./routes/user.routes');
@@ -7,14 +8,14 @@ const serviciosRoutes = require('./routes/servicios.routes');
 const authRoutes = require('./routes/auth.routes');
 const app = express();
 const mongoose = require('mongoose');
-require('dotenv').config();
 
 const corsOptions = {
     origin: ['http://localhost:5173'],
     optionsSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions));
+//comento temporalmente las opciones porque me dan un conlicto al momento de registrar usuario
+app.use(cors(/* corsOptions */));
 app.set('PORT', 3000);
 app.use(express.json());
 app.use(passport.initialize());
