312b3633f5a8751532740eaa738a8e361fde5e95
Ayelen Garcia <ayelengarcia7@hotmail.com>
2024-06-06 13:19:03 -0300

    Install express-session/MongoStore y toastify  agrego config back, agrego msgs de exito/error, styles y modifico context

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,8 @@ const serviciosRoutes = require('./routes/servicios.routes');
 const authRoutes = require('./routes/auth.routes');
 const app = express();
 const mongoose = require('mongoose');
+// const session = require("express-session");
+// const MongoStore = require("connect-mongo");
 require('dotenv').config();
 
 const corsOptions = {
@@ -26,15 +28,53 @@ app.use('/reservas', reservasRoutes);
 app.use('/servicios', serviciosRoutes);
 
 // Conexión al puerto 3000
-const port = app.get('PORT')
-app.listen(port, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);
-})
+ const port = app.get('PORT')
+ app.listen(port, () => {
+     console.log(`Servidor escuchando en http://localhost:${port}`);
+ })
 
-main().catch(err => console.log(err));
+ main().catch(err => console.log(err));
 
 // Conexión con MongoDB
-async function main() {
-    console.log(process.env.MONGO_URI);
-    await mongoose.connect(process.env.MONGO_URI);
-}
+ async function main() {
+     console.log(process.env.MONGO_URI);
+     await mongoose.connect(process.env.MONGO_URI);
+ }
+
+// const mongoUri =
+//   "mongodb+srv://ayelengarcia7:eIXUnjHpOu7NgSKF@clustercoder.t6a33ln.mongodb.net/?retryWrites=true&w=majority";
+// const dbName = "BDofiflex"
+// const PORT = "3000"
+
+// //Mongo session
+// app.use(
+//   session({
+//     store: MongoStore.create({
+//       mongoUrl: mongoUri,
+//       dbName: dbName,
+//       mongoOptions: {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       },
+//       ttl: 60 * 60 * 10000,
+//     }),
+//     secret: "secretmydbofiflex",
+//     resave: true,
+//     saveUninitialized: true,
+//   })
+// );
+
+// mongoose.set("strictQuery", false);
+
+// // Conexión con MongoDB
+// mongoose
+//   .connect(mongoUri, { dbName: dbName })
+//   .then(() => {
+//     console.log("DB conectada");
+//     app.listen(PORT, () => {
+//       console.log(`Servidor escuchando en http://localhost:${PORT}`);
+//     });
+//   })
+//   .catch((e) => {
+//     console.log("Error al conectar la DB", e);
+//   });
