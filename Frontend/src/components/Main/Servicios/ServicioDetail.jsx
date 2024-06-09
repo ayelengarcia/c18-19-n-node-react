2e7356b0bbb530d4c9e37f6d6da0e6ca014dd615
paiva73 <mau.paiva10@gmail.com>
2024-06-09 17:07:46 -0300

    add: función para reservar
    Agrego un custom hook para hacer la reserva de un servicio.

--- Frontend/src/components/Main/Servicios/ServicioDetail.jsx
+++ Frontend/src/components/Main/Servicios/ServicioDetail.jsx
@@ -2,25 +2,27 @@ import styles from './ServicioDetail.module.css'
 import { useParams } from 'react-router-dom';
 import { useContext } from 'react';
 import Context from '../../../context/context.jsx';
+import PostDataDetail from './PostDataDetail.js';
 
 const ServicioDetail = () => {
-  const { servicios } = useContext(Context);
-
+  const { servicios, usuario } = useContext(Context);
   const { id } = useParams();
   const servicio = servicios.find((servicio) => servicio.servicioID === id);
 
+  const {handleSubmit}  = PostDataDetail({servicio, usuario});
+
+
+
   return (
     <>
-    {console.log(servicio)}
-
       <div className={styles.container}>
         {/* Parte de imagenes */}
         <div className={styles.center_column}>
-          <img src='https://upload.wikimedia.org/wikipedia/commons/1/1b/Square_200x200.png' className={styles.img}/>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/1/1b/Square_200x200.png' className={styles.img} />
           <div className={styles.center_row}>
-            <img src='https://upload.wikimedia.org/wikipedia/commons/1/1b/Square_200x200.png' className={`${styles.img} ${styles.img_abajo}`}/>
-            <img src='https://upload.wikimedia.org/wikipedia/commons/1/1b/Square_200x200.png' className={`${styles.img} ${styles.img_abajo}`}/>
-            <img src='https://upload.wikimedia.org/wikipedia/commons/1/1b/Square_200x200.png' className={`${styles.img} ${styles.img_abajo}`}/>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/1/1b/Square_200x200.png' className={`${styles.img} ${styles.img_abajo}`} />
+            <img src='https://upload.wikimedia.org/wikipedia/commons/1/1b/Square_200x200.png' className={`${styles.img} ${styles.img_abajo}`} />
+            <img src='https://upload.wikimedia.org/wikipedia/commons/1/1b/Square_200x200.png' className={`${styles.img} ${styles.img_abajo}`} />
           </div>
         </div>
         {/* Parte de texto */}
@@ -33,7 +35,9 @@ const ServicioDetail = () => {
             <p className={styles.fecha_hora}>{servicio.fecha}</p>
             <p className={styles.fecha_hora}>{servicio.hora}</p>
           </div>
-          <button className={styles.btn}>Reservar</button>
+          <form onSubmit={handleSubmit}>
+            <button className={styles.btn}>Reservar</button>
+          </form>
         </div>
       </div>
     </>
