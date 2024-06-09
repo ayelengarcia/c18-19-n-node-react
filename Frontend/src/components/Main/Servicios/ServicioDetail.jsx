60bed6933a72a62e8a48fc9b3826a93b525af8e9
paiva73 <mau.paiva10@gmail.com>
2024-06-09 16:10:37 -0300

    style: ServicioDetails

--- Frontend/src/components/Main/Servicios/ServicioDetail.jsx
+++ Frontend/src/components/Main/Servicios/ServicioDetail.jsx
@@ -1,4 +1,4 @@
-import styles from './Servicios.module.css';
+import styles from './ServicioDetail.module.css'
 import { useParams } from 'react-router-dom';
 import { useContext } from 'react';
 import Context from '../../../context/context.jsx';
@@ -9,20 +9,34 @@ const ServicioDetail = () => {
   const { id } = useParams();
   const servicio = servicios.find((servicio) => servicio.servicioID === id);
 
-
   return (
-    <div className={styles.card} key={servicio._id}>
-      <img src={servicio.imagen} alt={servicio.titulo} />
-      <div>
-        <h2>{servicio.titulo}</h2>
-        <div className={styles.detalles}>
-          <p>{servicio.descripcion}</p>
-          <p>{servicio.disponible ? 'Disponible' : 'No disponible'}</p>
-          <p>{servicio.categoria}</p>
+    <>
+    {console.log(servicio)}
+
+      <div className={styles.container}>
+        {/* Parte de imagenes */}
+        <div className={styles.center_column}>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/1/1b/Square_200x200.png' className={styles.img}/>
+          <div className={styles.center_row}>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/1/1b/Square_200x200.png' className={`${styles.img} ${styles.img_abajo}`}/>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/1/1b/Square_200x200.png' className={`${styles.img} ${styles.img_abajo}`}/>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/1/1b/Square_200x200.png' className={`${styles.img} ${styles.img_abajo}`}/>
+          </div>
         </div>
-        <button className={styles.btn}>Reservar</button>
+        {/* Parte de texto */}
+        <div className={styles.container_text}>
+          <div>
+            <h1 className={styles.titulo}>{servicio.titulo}</h1>
+            <p>{servicio.descripcion} Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum provident ab quaerat illum adipisci accusamus aut, aperiam, beatae, consequuntur aliquam neque aspernatur doloribus facere porro magni dicta minus? Placeat, fugit.</p>
+          </div>
+          <div className={styles.center_row}>
+            <p className={styles.fecha_hora}>{servicio.fecha}</p>
+            <p className={styles.fecha_hora}>{servicio.hora}</p>
+          </div>
+          <button className={styles.btn}>Reservar</button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
